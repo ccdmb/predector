51feafb3d68b44c5a01644c177e674224ea9409a
Darcy Jones <darcy.ab.jonoes@gmail.com>
2021-07-15 15:45:45 +0800

    First attempt at conda packages for effectorp3 and signalp6

---
+++ conda/recipes/signalp6/1.0/unregister.sh
@@ -0,0 +1,7 @@
+# In here you should remove any (and only the) files that you installed in the register.sh script.
+# Note that we will automatically replace the placeholder script.
+
+# Make sure you use the -f variants to avoid errors if a file doesn't exist.
+
+python3 -m pip uninstall signalp6
+# An example is here for the signalp 5 recipe
