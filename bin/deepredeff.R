5b8dbaa8f0ed97601da6051f083e7b2bda274d94
Darcy Jones <darcy.ab.jones@gmail.com>
2021-11-05 05:54:54 +0000

    got deepredeff installer going. script to run it from command line

---
+++ bin/deepredeff.R
@@ -0,0 +1,80 @@
+#!/usr/bin/env Rscript
+
+VERSION = "0.1.1"
+VALID_TAXON = c("bacteria", "fungi", "oomycete")
+
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("deepredeff"))
+
+option_list <- list(
+    make_option(
+        c("-i", "--infile"),
+        type="character",
+        action="store",
+        help="The input aligned fasta (required)."
+    ),
+    make_option(
+        c("-o", "--outfile"),
+        type="character",
+        action="store",
+	default="",
+        help="The output file to write to (default: stdout)."
+    ),
+    make_option(
+        c("-t", "--taxon"),
+	type="character",
+	action="store",
+	default="fungi",
+	help="The model to use for deepredeff (default: fungi)."
+    ),
+    make_option(
+        "--version",
+        type="logical",
+        action="store_true",
+        default=FALSE,
+        help="Print version and exit.",
+    )
+)
+
+parser <- OptionParser(
+    usage = "%prog --infile in.fasta --outfile out.fasta",
+    option_list = option_list
+)
+
+args <- parse_args(parser)
+
+log_stderr <- function(...) {
+  cat(sprintf(...), sep='', file=stderr())
+}
+
+quit_with_err <- function(...) {
+  log_stderr(...)
+  quit(save = "no", status = 1, runLast = FALSE)
+}
+
+validate_file <- function(path) {
+  if (is.null(path)) {
+    quit_with_err("Please provide required file")
+  }
+}
+
+
+main <- function(args) {
+  if (args$version) {
+    cat(VERSION, file=stdout())
+    quit(save = "no", status = 0, runLast = FALSE)
+  }
+
+  if (!args$taxon %in% VALID_TAXON) {
+    quit_with_err(paste(c("--taxon must be one of", paste(VALID_TAXON, collapse = ", "))))
+  }
+
+  validate_file(args$infile)
+  validate_file(args$outfile)
+
+  capture.output(pred_result <- predict_effector(input = args$infile, taxon = args$taxon))
+
+  write.table(pred_result[, c("name", "s_score", "prediction")], file = args$outfile, sep = "\t", quote = FALSE, row.names = FALSE)
+}
+
+main(args)
